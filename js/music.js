297e862ee310c4a2824cf2831b70030791c39423
bignimbus <jdauriemma@gmail.com>
2014-04-21 09:59:31 -0400

    Loading dialog in music.html

--- js/music.js
+++ js/music.js
@@ -1,25 +1,26 @@
 
-	$.get('audio/musiclist.php', function(data){
-		// get all files from /audio/mp3
-		data = jQuery.parseJSON(data);
-		console.log(data);
-		var n;
-		var j;
-		var song = [];
-		for (n in data){
-			if (data[n][0].length > 5){
-				song.push(data[n]);
-			}
+$.get('audio/musiclist.php', function(data){
+	// get all files from /audio/mp3
+	data = jQuery.parseJSON(data);
+	var n, 
+	song = [];
+	for (n in data){
+		if (data[n][0].length > 5){
+			song.push(data[n]);
 		}
-		console.log(song);
-		song.sort();
+	}
 		
-		for(j=0; j<song.length; j++){
-			$("#playlist").append("<div class=song id=" + j + ">Loaded div " + j + "</div>");
-			$("#"+j).ready(function(){
-				console.log("div " + j + "ready.");
-				$("#"+j).html('<h3>' + song[j][1] + '</h3><audio controls preload="none"><source src="audio/mp3/' + song[j][0] + '" type="audio/mpeg"><source src="audio/ogg/' + song[j][0] + '" type="audio/ogg"><p>Your browser does not support the audio element.</p></audio><p><strong>Performer:</strong> ' + song[j][2] + '<br /><strong>My Role:</strong> ' + song[j][3] + '</p><br/>');
-			});
-		}
+	//keep consistent song loading order
+	song.sort();
 		
-	});
+	for( var j=0; j<song.length; j++){
+        // for each song, load a div with that song in it
+		$("#playlist").append("<div class=song id=" + j + ">Loaded div " + j + "</div>");
+		$("#"+j).ready(function(){
+			$("#"+j).html('<h3>' + song[j][1] + '</h3><audio controls preload="none"><source src="audio/mp3/' + song[j][0] + '" type="audio/mpeg"><source src="audio/ogg/' + song[j][0] + '" type="audio/ogg"><p>Your browser does not support the audio element.</p></audio><p><strong>Performer:</strong> ' + song[j][2] + '<br /><strong>My Role:</strong> ' + song[j][3] + '</p><br/>');
+		});
+		
+		//once loop is done, remove loading screen
+		if ($("#loading") && j+1 === song.length) $("#loading").remove();
+	}
+});
