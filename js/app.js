9983b8fde26d9e24dd45b4811c793c2d24b6f801
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-01-09 10:12:10 -0600

    init parent view

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-require(['router', 'models/main'],
-function (Router, ParentModel) {
+require(['router', 'models/main', 'views/main'],
+function (Router, ParentModel, ParentView) {
     'use strict';
     var app = {};
     app.model = new ParentModel();
     app.router = new Router({"model": app.model});
+    app.view = new ParentView({
+        "model": app.model,
+        "router": app.router
+    });
 
     Backbone.history.start();
 
