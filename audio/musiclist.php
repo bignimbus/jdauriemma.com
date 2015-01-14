298dccef084c2c5fdaddcacb6ee160f73fb4af24
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-01-14 14:45:31 -0600

    remove extensions from file returns

--- audio/musiclist.php
+++ audio/musiclist.php
@@ -1,21 +1,19 @@
 <?php
 require_once('../php/getID3-1.9.7/getid3/getid3.php');
 $getID3 = new getID3;
-	$dir = './mp3';
-	$music = scandir($dir);
-	$title = array();
-	$comment = array();
-	$musicinfo = array();
-	$filename = array();
-	$playlist = array();
-		foreach($music as $key=>$value) {
-			$fileinfo = $getID3->analyze("./mp3/".$music[$key]);
-			$playlist[$key] = array(
-				"file" => $music[$key],
-				"title" => $fileinfo['tags']['id3v1']['title'][0],
-				"performer" => $fileinfo['tags']['id3v1']['artist'][0],
-				"role" => $fileinfo['tags']['id3v1']['comment'][0]
-			 );
-		}
-	echo json_encode($playlist);
+$dir = './mp3';
+$music = scandir($dir);
+$playlist = array();
+
+foreach($music as $key=>$value) {
+	$fileinfo = $getID3->analyze("./mp3/".$music[$key]);
+	$playlist[$key] = array(
+		"file" => substr_replace($music[$key], '', -4),
+		"title" => $fileinfo['tags']['id3v1']['title'][0],
+		"performer" => $fileinfo['tags']['id3v1']['artist'][0],
+		"role" => $fileinfo['tags']['id3v1']['comment'][0]
+	 );
+}
+
+echo json_encode($playlist);
 ?>
