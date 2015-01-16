75dc7b5cf748cc4c449bb40fe2826952bc967919
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-01-16 13:21:51 -0600

    style and layout tweaks for code and social views

--- js/templates/code.js
+++ js/templates/code.js
@@ -22,23 +22,23 @@ return Handlebars.template({"1":function(depth0,helpers,partials,data) {
   return buffer;
 },"3":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "            <li class=\"owner-info\"><a href=\"https://github.com/"
-    + escapeExpression(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"login","hash":{},"data":data}) : helper)))
+  return "            <li class=\"owner-info\"><a href=\""
+    + escapeExpression(((helper = (helper = helpers.html_url || (depth0 != null ? depth0.html_url : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"html_url","hash":{},"data":data}) : helper)))
     + "\">"
     + escapeExpression(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"login","hash":{},"data":data}) : helper)))
-    + "</a></li>\n            <li><a href=\"https://github.com/"
-    + escapeExpression(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"login","hash":{},"data":data}) : helper)))
+    + "</a></li>\n            <li><a href=\""
+    + escapeExpression(((helper = (helper = helpers.html_url || (depth0 != null ? depth0.html_url : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"html_url","hash":{},"data":data}) : helper)))
     + "\"><img class=\"avatar\" src=\""
     + escapeExpression(((helper = (helper = helpers.avatar_url || (depth0 != null ? depth0.avatar_url : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"avatar_url","hash":{},"data":data}) : helper)))
     + "\"></a></li>\n";
 },"5":function(depth0,helpers,partials,data) {
   var stack1, lambda=this.lambda, escapeExpression=this.escapeExpression;
-  return "            <li class=\"owner-info\"><a href=\"https://github.com/"
-    + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.repo : depth0)) != null ? stack1.owner : stack1)) != null ? stack1.login : stack1), depth0))
+  return "            <li class=\"owner-info\"><a href=\""
+    + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.repo : depth0)) != null ? stack1.owner : stack1)) != null ? stack1.html_url : stack1), depth0))
     + "\">"
     + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.repo : depth0)) != null ? stack1.owner : stack1)) != null ? stack1.login : stack1), depth0))
-    + "</a></li>\n            <li><a href=\"https://github.com/"
-    + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.repo : depth0)) != null ? stack1.owner : stack1)) != null ? stack1.login : stack1), depth0))
+    + "</a></li>\n            <li><a href=\""
+    + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.repo : depth0)) != null ? stack1.owner : stack1)) != null ? stack1.html_url : stack1), depth0))
     + "\"><img class=\"avatar\" src=\""
     + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.repo : depth0)) != null ? stack1.owner : stack1)) != null ? stack1.avatar_url : stack1), depth0))
     + "\"></a></li>\n";
@@ -50,16 +50,20 @@ return Handlebars.template({"1":function(depth0,helpers,partials,data) {
 },"8":function(depth0,helpers,partials,data) {
   var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda, buffer = "    <div id=\""
     + escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"id","hash":{},"data":data}) : helper)))
-    + "\" class=\"gist code\">\n        <ul>\n            <li class=\"owner-info\">"
+    + "\" class=\"gist code\">\n        <ul>\n            <li class=\"owner-info\"><a href=\""
+    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.owner : depth0)) != null ? stack1.html_url : stack1), depth0))
+    + "\">"
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.owner : depth0)) != null ? stack1.login : stack1), depth0))
-    + "</li>\n            <li><a href=\"https://github.com/"
-    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.owner : depth0)) != null ? stack1.login : stack1), depth0))
+    + "</a></li>\n            <li><a href=\""
+    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.owner : depth0)) != null ? stack1.html_url : stack1), depth0))
     + "\"><img class=\"avatar\" src=\""
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.owner : depth0)) != null ? stack1.avatar_url : stack1), depth0))
-    + "\"></a></li>\n            <li class=\"owner-info\">";
+    + "\"></a></li>\n            <li class=\"owner-info\"><a href=\""
+    + escapeExpression(((helper = (helper = helpers.html_url || (depth0 != null ? depth0.html_url : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"html_url","hash":{},"data":data}) : helper)))
+    + "\">";
   stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.files : depth0), {"name":"each","hash":{},"fn":this.program(9, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
-  return buffer + "</li>\n            <li>"
+  return buffer + "</a></li>\n            <li>"
     + escapeExpression(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"description","hash":{},"data":data}) : helper)))
     + "</li>\n            <li class=\"updated\">Updated "
     + escapeExpression(((helpers.format_date || (depth0 && depth0.format_date) || helperMissing).call(depth0, (depth0 != null ? depth0.updated_at : depth0), "YYYY-MM-DD[T]HH:mm:ss[Z]", {"name":"format_date","hash":{},"data":data})))
@@ -69,7 +73,7 @@ return Handlebars.template({"1":function(depth0,helpers,partials,data) {
   return escapeExpression(((helper = (helper = helpers.filename || (depth0 != null ? depth0.filename : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"filename","hash":{},"data":data}) : helper)))
     + " ";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1, buffer = "<p>\nI began coding at age 12 when my friend and I started our own business making websites for local businesses.  Recently, I have added more advanced JavaScript, PHP, jQuery and Java ME skills to my repertoire.  Here are some recent projects of mine.\n</p>\n<div id=\"repos\">\n<h2>Repos</h2>\n";
+  var stack1, buffer = "<p>\nI enjoy working with: JavaScript, jQuery, underscore, Backbone, handlebars, jasmine, node/npm, grunt, git, html5, less, and css.  See below for my code and my contributions to others' projects.\n</p>\n<div id=\"repos\">\n<h2>Repos</h2>\n";
   stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.repos : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
   buffer += "</div>\n<div id=\"gists\">\n<h2>Gists</h2>\n";
--- js/templates/social.js
+++ js/templates/social.js
@@ -1,7 +1,7 @@
 define(['helper'], function(Handlebars) {
 
 return Handlebars.template({"1":function(depth0,helpers,partials,data) {
-  var stack1, helper, lambda=this.lambda, escapeExpression=this.escapeExpression, functionType="function", helperMissing=helpers.helperMissing, buffer = "<ul>\n    <li><img src=\""
+  var stack1, helper, lambda=this.lambda, escapeExpression=this.escapeExpression, functionType="function", helperMissing=helpers.helperMissing, buffer = "<ul>\n    <li><img class=\"avatar\" src=\""
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.originalUser : depth0)) != null ? stack1.profile_image_url : stack1), depth0))
     + "\"><a href=\"https://twitter.com/"
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.originalUser : depth0)) != null ? stack1.name : stack1), depth0))
@@ -23,10 +23,14 @@ return Handlebars.template({"1":function(depth0,helpers,partials,data) {
   if (stack1 != null) { buffer += stack1; }
   return buffer + "</ul>\n";
 },"2":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda;
   return "        <li><img src=\""
     + escapeExpression(((helper = (helper = helpers.media_url || (depth0 != null ? depth0.media_url : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"media_url","hash":{},"data":data}) : helper)))
-    + "\" class=\"avatar\"></li>\n";
+    + "\" width=\""
+    + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.sizes : depth0)) != null ? stack1.small : stack1)) != null ? stack1.w : stack1), depth0))
+    + "\" height=\""
+    + escapeExpression(lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.sizes : depth0)) != null ? stack1.small : stack1)) != null ? stack1.h : stack1), depth0))
+    + "\"></li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var stack1, buffer = "<br/>\n<div style=\"margin:0 auto; text-align:center;\">\n<a href=\"https://www.facebook.com/jdauriemma\"><img src=\"images/facebook.png\" style=\"display:inline-block;margin: 0em 1em 1em 1em;\"/></a>\n<a href=\"https://www.twitter.com/jbones3000\"><img src=\"images/twitter.png\" style=\"display:inline-block;margin: 0em 1em 1em 1em;\"/></a>\n<a href=\"http://www.linkedin.com/in/jeffreyauriemma/\"><img src=\"images/linkedin.png\" style=\"display:inline-block;margin: 0em 1em 1em 1em;\"/></a>\n</div>\n<h2>Recent Tweets</h2>\n<div id = \"feed\">\n</div>\n";
   stack1 = helpers.each.call(depth0, depth0, {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
