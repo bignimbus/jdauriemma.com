14d1901e6a971d843925149d26abeb13661191d2
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-01-20 16:26:21 -0600

    use pushState instead of hash routes

--- js/app.js
+++ js/app.js
@@ -7,6 +7,14 @@ function (Router, NavigationView) {
     var app = {},
         navbar = new NavigationView();
     app.router = new Router();
-    Backbone.history.start();
+    Backbone.history.start({"pushState": true});
+    $(document).on('click', 'a:not([data-bypass])', function (evt) {
+        var href = $(this).attr('href'),
+            protocol = this.protocol + '//';
+        if (href.slice(protocol.length) !== protocol) {
+            evt.preventDefault();
+            app.router.navigate(href, true);
+        }
+    });
     return app.router;
 });
