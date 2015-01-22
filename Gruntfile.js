bdc4e243236a19f0a3001beaa3af5c74bcda5137
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-01-22 11:48:36 -0600

    add new dev tools for local server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var pushState = require('grunt-connect-pushstate/lib/utils').pushState,
+    rewrite = require('connect-modrewrite'),
+    rewriteRulesSnippet = require('grunt-connect-rewrite/lib/utils').rewriteRequest;
 module.exports = function (grunt) {
     grunt.initConfig({
         "less": {
@@ -20,6 +23,46 @@ module.exports = function (grunt) {
                 "ext": '.js'
             }
         },
+        "connect": {
+            "server": {
+                "rules": [
+                    // Internal rewrite
+                    {"from": '^/', "to": './'},
+                    // Internal rewrite
+                    // {"from": '^/js/(.*)$', "to": '/src/js/$1'},
+                ],
+                "options": {
+                    "hostname": 'localhost',
+                    // "port": 3000,
+                    "base": '.',
+                    "debug": true,
+                    "keepalive": true,
+                    "middleware": function(connect, options, middlewares) {
+
+                        // the rules that shape our mod-rewrite behavior
+                        var middlewares = [];
+
+                    // RewriteRules support
+                    middlewares.push(rewriteRulesSnippet);
+
+                    if (!Array.isArray(options.base)) {
+                        options.base = [options.base];
+                    }
+
+                    var directory = options.directory || options.base[options.base.length - 1];
+                    options.base.forEach(function (base) {
+                        // Serve static files.
+                        middlewares.push(connect.static(base));
+                    });
+
+                    // Make directory browse-able.
+                    middlewares.push(connect.directory(directory));
+
+                    return middlewares;
+                    }
+                }
+            }
+        },
         // "jasmine": {
         //     "pivotal": {
         //         "src": 'src/*.js',
@@ -37,13 +80,15 @@ module.exports = function (grunt) {
             "dist": {
                 "options": {
                     "port": 5000,
-                    "keepalive": true
+                    "keepalive": true,
+                    "base": "./"
                 }
             }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-php');
     grunt.registerTask('default', ['less', 'handlebars', 'php']);
 };
