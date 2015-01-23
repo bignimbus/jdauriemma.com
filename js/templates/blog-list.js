09c767a36aa134a1dd79614b1e668166f8dd8393
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-01-23 11:36:49 -0600

    validate data before render

--- js/templates/blog-list.js
+++ js/templates/blog-list.js
@@ -1,7 +1,12 @@
 define(['helper'], function(Handlebars) {
 
 return Handlebars.template({"1":function(depth0,helpers,partials,data) {
-  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "    <ul>\n        <li class=\"hero\">";
+  var stack1;
+  stack1 = helpers['if'].call(depth0, (depth0 != null ? depth0['regular-title'] : depth0), {"name":"if","hash":{},"fn":this.program(2, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { return stack1; }
+  else { return ''; }
+  },"2":function(depth0,helpers,partials,data) {
+  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "\n    <ul>\n        <li class=\"hero\">";
   stack1 = ((helper = (helper = helpers.heroImage || (depth0 != null ? depth0.heroImage : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"heroImage","hash":{},"data":data}) : helper));
   if (stack1 != null) { buffer += stack1; }
   buffer += "</li>\n        <li class=\"text\"><a href=\"blog_"
@@ -11,14 +16,14 @@ return Handlebars.template({"1":function(depth0,helpers,partials,data) {
     + "</h4></a>\n            <p>";
   stack1 = ((helper = (helper = helpers.snippet || (depth0 != null ? depth0.snippet : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"snippet","hash":{},"data":data}) : helper));
   if (stack1 != null) { buffer += stack1; }
-  return buffer + "</p>\n        </li>\n    </ul>\n";
+  return buffer + "</p>\n        </li>\n    </ul>\n    ";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "<div class=\"post-list\">\n    <h2>"
     + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
-    + "</h2>\n";
+    + "</h2>\n    ";
   stack1 = helpers.each.call(depth0, depth0, {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
-  return buffer + "</div>";
+  return buffer + "\n</div>";
 },"useData":true})
 
 });
