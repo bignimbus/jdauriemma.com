7c6f061a3be569cc3ce4e95fa83cb80210f9a219
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-01-23 23:24:05 -0600

    change relative path

--- js/templates/nav.js
+++ js/templates/nav.js
@@ -1,7 +1,7 @@
 define(['helper'], function(Handlebars) {
 
 return Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<nav>\n    <li class=\"select\"><a id=\"blog\" class=\"menuitem\" href=\"/blog\">blog</a></li>\n    <li class=\"select\"><a id=\"code\" class=\"menuitem\" href=\"/code\">code</a></li>\n    <li class=\"select\"><a id=\"music\" class=\"menuitem\" href=\"/music\">music</a></li>\n    <li class=\"select\"><a id=\"social\" class=\"menuitem\" href=\"/social\">social</a></li>\n</nav>";
+  return "<nav>\n    <li class=\"select\"><a id=\"blog\" class=\"menuitem\" href=\"blog\">blog</a></li>\n    <li class=\"select\"><a id=\"code\" class=\"menuitem\" href=\"code\">code</a></li>\n    <li class=\"select\"><a id=\"music\" class=\"menuitem\" href=\"music\">music</a></li>\n    <li class=\"select\"><a id=\"social\" class=\"menuitem\" href=\"social\">social</a></li>\n</nav>";
   },"useData":true})
 
 });
