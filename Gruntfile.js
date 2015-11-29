57f198fee4e026248057caa01d300cca500d2639
bignimbus <jdauriemma@gmail.com>
2015-11-29 16:42:46 -0600

    optimize build process and use cdn for js libraries

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,17 @@ var pushState = require('connect-pushstate'),
     rewriteRulesSnippet = require('grunt-connect-rewrite/lib/utils').rewriteRequest;
 module.exports = function (grunt) {
     grunt.initConfig({
+        "uglify": {
+            "my_target": {
+                "files": {
+                    "dist/app.min.js": ["dist/app.js"]
+                }
+            }
+        },
         "less": {
             "development": {
                 "files": {
                     "styles/style.css": "styles/main.less"
-                }
-            }
-        },
-        "webpack": {
-            "default": {
-                //"context": __dirname + "/js",
-                "inline": true,
-                "entry": "./js/app.js",
-                "output": {
-                    "path": "./dist",
-                    "filename": "app.js",
-                    "library": "app"
                 }
             }
         },
@@ -35,6 +30,26 @@ module.exports = function (grunt) {
                 "expand": true,
                 "src": ['js/templates/*.hbs'],
                 "ext": '.js'
+            }
+        },
+        "requirejs": {
+            "main": {
+                "options": {
+                    'findNestedDependencies': true,
+                    'baseUrl': 'js',
+                    'optimize': 'none',
+                    'include': ['app.js'],
+                    'out': 'dist/app.js',
+                    'onModuleBundleComplete': function (data) {
+                        var fs = require('fs'),
+                        amdclean = require('amdclean'),
+                        outputFile = data.path;
+
+                        fs.writeFileSync(outputFile, amdclean.clean({
+                            'filePath': outputFile
+                        }));
+                    }
+                }
             }
         },
         "connect": {
@@ -56,23 +71,23 @@ module.exports = function (grunt) {
                         // the rules that shape our mod-rewrite behavior
                         var middlewares = [];
 
-                    // RewriteRules support
-                    middlewares.push(rewriteRulesSnippet);
+                        // RewriteRules support
+                        middlewares.push(rewriteRulesSnippet);
 
-                    if (!Array.isArray(options.base)) {
-                        options.base = [options.base];
-                    }
+                        if (!Array.isArray(options.base)) {
+                            options.base = [options.base];
+                        }
 
-                    var directory = options.directory || options.base[options.base.length - 1];
-                    options.base.forEach(function (base) {
-                        // Serve static files.
-                        middlewares.push(connect.static(base));
-                    });
+                        var directory = options.directory || options.base[options.base.length - 1];
+                        options.base.forEach(function (base) {
+                            // Serve static files.
+                            middlewares.push(connect.static(base));
+                        });
 
-                    // Make directory browse-able.
-                    middlewares.push(connect.directory(directory));
+                        // Make directory browse-able.
+                        middlewares.push(connect.directory(directory));
 
-                    return middlewares;
+                        return middlewares;
                     }
                 }
             }
@@ -100,10 +115,12 @@ module.exports = function (grunt) {
             }
         }
     });
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-webpack');
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-php');
-    grunt.registerTask('default', ['less', 'handlebars', 'php']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['less', 'handlebars', 'requirejs', 'uglify', 'php']);
 };
